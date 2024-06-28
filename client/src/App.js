ad8bba35246c9e4bd404c450e9d8dfe4d088db45
Kshitij Satpathy <akuma10901@gmail.com>
2024-06-28 22:56:44 +0530

    created template for client-side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Blog from "./Blog";
+import {Fragment} from "react";
+import Footer from "./Footer";
+
+function App() {
+  return (
+        <Blog/>
+  );
+}
+
+export default App;
